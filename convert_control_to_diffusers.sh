4f7cd2efbea23f495a9a71c922d0434a50d54642
JoonHyun <joonhyun08@anipen.com>
2023-03-20 14:56:06 +0900

    control inpainting

---
+++ convert_control_to_diffusers.sh
@@ -0,0 +1,4 @@
+python diffusers/scripts/convert_controlnet_to_diffusers.py \
+    --checkpoint_path /data3/model_checkpoints/DIFFUSION_DB/ControlNet/control_sd15_openpose.pth\
+    --original_config_file /workspace/ControlNet_jh/ControlNet/models/cldm_v15.yaml\
+    --dump_path /data3/model_checkpoints/DIFFUSION_DB/ControlNet/diffusers/control_sd15_openpose
